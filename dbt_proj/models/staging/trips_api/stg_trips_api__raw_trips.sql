c598255436be121facb3b74fd9c652f9d9a9ed66
Ilia Khrenov <ilia.khrenov@gmail.com>
2023-03-31 10:26:20 +0400

    trip_api staging model

--- dbt_proj/models/staging/trips_api/stg_trips_api__raw_trips.sql
+++ dbt_proj/models/staging/trips_api/stg_trips_api__raw_trips.sql
@@ -8,7 +8,29 @@ source as (
 
 final as (
 
-    select * from source
+    select
+        trip_id,
+        taxi_id,
+        parse_timestamp('%Y-%m-%dT%H:%M:%E*S', trip_start_timestamp) as trip_start_dt,
+        parse_timestamp('%Y-%m-%dT%H:%M:%E*S', trip_end_timestamp) as trip_end_dt,
+        safe_cast(trip_seconds as float64) as trip_duration_seconds,
+        safe_cast(trip_miles as float64) as trip_length_miles,
+        safe_cast(pickup_community_area as int) as pickup_community_area,
+        safe_cast(dropoff_community_area as int) as dropoff_community_area,
+        safe_cast(fare as float64) as fare_usd,
+        safe_cast(tips as float64) as tips_usd,
+        safe_cast(tolls as float64) as tolls_usd,
+        safe_cast(extras as float64) as extra_charges_usd,
+        safe_cast(trip_total as float64) as trip_total_cost_usd,
+        payment_type,
+        company as taxi_company,
+        safe_cast(pickup_centroid_latitude as float64) as pickup_centroid_latitude,
+        safe_cast(pickup_centroid_longitude as float64) as pickup_centroid_longitude,
+        safe_cast(dropoff_centroid_latitude as float64) as dropoff_centroid_latitude,
+        safe_cast(dropoff_centroid_longitude as float64) as dropoff_centroid_longitude,
+        pickup_census_tract,
+        dropoff_census_tract
+    from source
 
 )
 
